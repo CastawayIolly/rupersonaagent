fcfb2f466c8620863122a776c2e5d09c77a1602c
IollyM <anastasiamatveevaitmo@gmail.com>
2023-02-17 18:53:04 +0300

    add structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as requirement_file:
+    requirements = requirement_file.read().split()
+
+setup(
+    name="RuPersonaAgent",
+    version="1.0.0",
+    install_requires=requirements,
+)
