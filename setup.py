b768871286b019863913ea20f2f9837cede5bb43
IollyM <anastasiamatveevaitmo@gmail.com>
2023-08-22 08:26:02 +0300

    resolve

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import sys
 import subprocess
 from setuptools import setup, find_packages
-import yaml
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", "PyYAML"])
+
+import yaml
 
 with open("requirements.yml") as file_handle:
     environment_data = yaml.full_load(file_handle)
@@ -12,6 +13,10 @@ setup(
     name="RuPersonaAgent",
     version="1.0.0",
     install_requires=environment_data['dependencies'][23]["pip"],
-    packages=find_packages(include=['generative_model', 'speech_extraction'])
+    packages=find_packages(include=['generative_model', 'speech_extraction',
+                                    'inference_optimization', 'internet_memory_model',
+                                    'knowledge_distillation', 'personification',
+                                    'rule_based_information_extraction',
+                                    'tests', 'datasets'])
 
 )
