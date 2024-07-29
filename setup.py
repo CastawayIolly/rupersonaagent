6c4f7fed1f2f0101f6ba8179dcea47be77d1fdf3
IollyM <anastasiamatveevaitmo@gmail.com>
2024-07-30 01:04:40 +0300

    change python version

--- setup.py
+++ setup.py
@@ -125,7 +125,9 @@ setup(
         'lightning==2.2.5',
         'accelerate==0.31.0',
         'ctransformers==0.2.27',
-        'gradio==4.39.0',
+        'gradio==4.36.1',
+        'unstructured==0.14.5',
+        'evaluate==0.4.2',
         'pandas==1.5.1',
     ],
     packages=find_packages(include=[
