2782729a744cab971f4baecbf512639b23c0e334
IollyM <anastasiamatveevaitmo@gmail.com>
2024-07-30 00:00:59 +0300

    fix pep8

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
         'tqdm==4.62.1',
         'transformers==4.24.0',
         'typing-extensions==4.6.1',
-        'urllib3==1.26.12',
+        'urllib3==2.0',
         'werkzeug==2.2.2',
         'xxhash==3.3.0',
         'yarl==1.8.1',
